85b914ca8b8f72b2dba12789598e170dbd03cbc9
Piotr Pęczkowski <peczkowski.piotrek@gmail.com>
2022-11-30 00:34:36 +0100

    create ModalBottomSheetUpdate widget

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -90,7 +90,7 @@ class ModalBottomSheetAdd {
                     ),
                   ),
                 ),
-                //TEXT FIELD - NAZWA RESTAURACJI
+                //TEXT FIELD - NAZWA ZADANIA
                 Padding(
                   padding: const EdgeInsets.only(bottom: 6, top: 10),
                   child: Theme(
@@ -140,6 +140,120 @@ class ModalBottomSheetAdd {
                       Navigator.of(context).pop();
                     },
                     child: Text('Dodaj',
+                        style: GoogleFonts.lato(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        )),
+                  ),
+                ),
+                //PRZYCISK ANULUJ
+                Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.orange.withOpacity(0.9)),
+                    child: Text(
+                      'Anuluj',
+                      style: GoogleFonts.lato(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class ModalBottomSheetUpdate {
+  static void updatePosition(context) {
+    showModalBottomSheet(
+      backgroundColor: const Color.fromARGB(255, 240, 240, 240),
+      isScrollControlled: true,
+      isDismissible: false,
+      context: context,
+      builder: (BuildContext context) {
+        return SizedBox(
+          height: MediaQuery.of(context).size.height * 0.7,
+          child: Padding(
+            padding: EdgeInsets.only(
+                top: 15,
+                left: 10,
+                right: 10,
+                bottom: MediaQuery.of(context).viewInsets.bottom + 15),
+            child: ListView(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 25, top: 10),
+                  child: Text(
+                    'Edytuj bieżące zadanie:',
+                    style: GoogleFonts.lato(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                //TEXT FIELD - NAZWA ZADANIA
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 6, top: 10),
+                  child: Theme(
+                    data: Theme.of(context).copyWith(
+                        colorScheme: ThemeData()
+                            .colorScheme
+                            .copyWith(primary: Colors.orange)),
+                    child: TextField(
+                      maxLength: 35,
+                      style: GoogleFonts.lato(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                      decoration: InputDecoration(
+                        label: Text(
+                          'Edytuj treść treść',
+                          style: GoogleFonts.lato(
+                              fontSize: 16,
+                              fontWeight: FontWeight.normal,
+                              color: Colors.black),
+                        ),
+                        filled: true,
+                        fillColor: Colors.white,
+                        focusColor: Colors.orange,
+                        prefixIcon: const Icon(Icons.edit_note),
+                        focusedBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(
+                            width: 1,
+                            color: Colors.orange,
+                          ),
+                        ),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5)),
+                      ),
+                      controller: null,
+                    ),
+                  ),
+                ),
+                //PRZYCISK DODAJ
+                Padding(
+                  padding: const EdgeInsets.only(top: 0),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.orange.withOpacity(0.9),
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text('Zaktualizuj',
                         style: GoogleFonts.lato(
                           fontSize: 16,
                           fontWeight: FontWeight.bold,
