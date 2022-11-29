58ff059c00c53bc289da46704bbe03f77a82c939
Piotr Pęczkowski <peczkowski.piotrek@gmail.com>
2022-11-29 22:27:39 +0100

    add home page navigation logic

--- lib/splash_screen/splash_screen.dart
+++ lib/splash_screen/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:my_todo_app/home_page/home_page.dart';
 import 'package:my_todo_app/main.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -12,6 +13,17 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
+    Future.delayed(
+      const Duration(seconds: 4),
+      () => {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const HomePage(),
+          ),
+        )
+      },
+    );
     return Scaffold(
       body: Stack(
         fit: StackFit.expand,
