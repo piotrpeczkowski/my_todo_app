620ba147f19a89f6e8a8b4d4d97c2b84778a38d4
Piotr Pęczkowski <peczkowski.piotrek@gmail.com>
2022-12-31 03:26:46 +0100

    organize imports in widgets.dart

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,9 +1,5 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:intl/intl.dart';
-import 'package:my_todo_app/home_page/cubit/home_page_cubit.dart';
 
 class NoteWidget extends StatelessWidget {
   const NoteWidget({
